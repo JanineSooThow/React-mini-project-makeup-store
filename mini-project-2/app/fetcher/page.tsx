1a02abdab32741d4f973f10b3f59eebcc050b444
JanineSooThow <j9soothow@gmail.com>
2023-06-17 00:46:00 +1200

    update

---
+++ mini-project-2/app/fetcher/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+import useSWR from 'swr';
+//http://localhost:3000/fetcher
+export default function Makeup() {
+  const fetcher = (url:string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR('https://makeup-api.herokuapp.com/api/v1/products.json', fetcher);
+
+  if (error) {
+    return <h2>An error has occurred</h2>;
+  }
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+     {data.map((products:any)=>{
+        return <p key={products.id}>{products.name}</p> //fetching entire array
+     })}
+      
+    </>
+  );
+}
---
+++ mini-project-2/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from '../src/components/Header'
+
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+
