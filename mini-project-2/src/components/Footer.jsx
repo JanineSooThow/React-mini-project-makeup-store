a343495b834c04735fde131ce6773db329406d92
JanineSooThow <j9soothow@gmail.com>
2023-06-24 09:04:44 +1200

    saturday mane

--- mini-project-2/src/components/Footer.jsx
+++ mini-project-2/src/components/Footer.jsx
@@ -1,60 +1,115 @@
-import React from "react"
+import React from "react";
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn} from 'mdb-react-ui-kit';
 
-const Footer = () => <footer className="page-footer font-small blue pt-4">
-    <div className="container-fluid text-center text-md-left">
-        <div className="row">
-            <div className="col-md-0 mt-md-0 mt-3">
-                <h5 className="text-uppercase">MAKEUP MIRAGE</h5>
-                <p>Enhance your confidence and radiance with high-quality cosmetics for every occasion</p>
-            </div>
+export default function Footer() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-dark text-lg-left'>
+      <MDBContainer className='p-4 pb-0'>
+        <form action=''>
+          <MDBRow className='d-flex justify-content-center'>
+            <MDBCol size='auto' className='mb-4 mb-md-0'>
+              <p className='pt-2'>
+                <strong>Sign up for our newsletter</strong>
+              </p>
+            </MDBCol>
+            
+            <MDBCol md='5' size='12' className='mb-4 mb-md-0'>
+                <MDBInput type='text' id='form5Example2' input="email" placeholder="Email" value="email" />
+            </MDBCol>
 
-            <hr className="clearfix w-100 d-md-none pb-0"/>
+           {/*Subscribe button*/}
+            <MDBCol size='auto' className='mb-4 mb-md-0'>
+              <MDBBtn outline color='dark'>
+                Subscribe
+              </MDBBtn>
+            </MDBCol>
+          </MDBRow>
+        </form>
+      </MDBContainer>
 
-            <div className="col-md-0 mt-md-0 mt-3">
-                <form className='d-flex justify-content-center'>
-                    <p className='pt-2'>
-                        <strong>Sign up for our newsletter</strong>
-                    </p>
-                        <input type="email" placeholder="Email" />
-                        <button type="submit">Subscribe</button>
-                </form>
-            </div>
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                MAKEUP MIRAGE
+              </h6>
+              <p>
+                Enhance your confidence and radiance with high-quality cosmetics for every occasion
+              </p>
+            </MDBCol>
 
+          {/*ABOUT Column */}
+            <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>ABOUT</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  About Us 
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Stores
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Join the team
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Ts & Cs
+                </a>
+              </p>
+            </MDBCol>
 
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">ABOUT</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">About Us</a></li>
-                    <li><a href="#!">Stores</a></li>
-                    <li><a href="#!">Join the team</a></li>
-                    <li><a href="#!">Ts & Cs </a></li>
-                </ul>
-            </div>
+          {/*CUSTOMER CARE column*/}
+            <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>CUSTOMER CARE</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  The Blog
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  FAQs
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Shipping Info
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Returns
+                </a>
+              </p>
+            </MDBCol>
 
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">CUSTOMER CARE</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">The Blog</a></li>
-                    <li><a href="#!">FAQs</a></li>
-                    <li><a href="#!">Shipping Info</a></li>
-                    <li><a href="#!">Returns</a></li>
-                    
-                </ul>
-            </div>
+          {/*CONTACT Column*/}
+            <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>CONTACT</h6>
+              <p>
+                Auckland, AKL NZ
+              </p>
+              <p>
+                info@makeupmirage.com
+              </p>
+              <p>
+                + 64 177 777 77
+              </p>
+        
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
 
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">CONTACT</h5>
-                <ul className="list-unstyled">
-                    <li>info@makeupmirage.com</li>
-                    <li>+ 64 123 456 789 </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <div className="footer-copyright text-center py-3">© Makeup Mirage 2023: Coded by Janine Soo Thow
-    </div>
-
-</footer>
-
-export default Footer
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2023 Makeup Mirage
+      </div>
+    </MDBFooter>
+  );
+}
