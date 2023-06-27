27cefa28e4a8859b95056b87c042a5901dbf599a
JanineSooThow <j9soothow@gmail.com>
2023-06-27 21:49:58 +1200

    tuesday update

--- mini-project-2/app/products/page.tsx
+++ mini-project-2/app/products/page.tsx
@@ -1,20 +1,25 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 //simplifies data fetching
+//imports the 'useSWR' hook from the swr library. SWR is a React hook for data fetching that simplifies asynchronous data fetching.
 import useSWR from "swr";
 
+//The 'fetcher' function takes a URL as input, performs a GET reuest to that URL using the 'fetch' function and returns the JSON response of that request.
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-//useEffect allows fetching of data from an API
+//Declaring a React functional component called 'Allproducts'
 function Allproducts() {
-  // const [products, setProducts] = useState([]);
 
+//using the 'useSWR' hook to fetch data from the specified URL using the 'fetcher' function defined earlier
   const { data, error, isLoading } = useSWR(
     "https://makeup-api.herokuapp.com/api/v1/products.json",
     fetcher
   );
 
+//This condition checks if there is an error fetching the data. If 'error' is truthy it returns a heading element displaying the error message.
   if (error) {
     return <h2>An error has occurred</h2>;
   }
@@ -23,19 +28,28 @@ function Allproducts() {
     return <h2>Loading...</h2>;
   }
 
+//The {!isLoading && data.map(...)} is a conditional response. It checks if 'isLoading' is falsy and then proceeds to render the content inside the curly braces. It maps over the 'data' array and returns a paragraph (<p>) element for each element in the 'data' array
   return (
     <div>
-      All products page here
       {!isLoading &&
         data.map((products: any) => {
           return (
-          <p>
-            {/* Insert cards here */}
-            {products.brand}
-          </p>);
-        })}
+            <div className="card-container">
+              <Card className ="products-card">
+                <Card.Img variant="top" style={{width: '200px', height: '250px'}} className ="product-image">{products.image_link}</Card.Img>
+                  <Card.Body>
+                    <Card.Title className ="card-title">{products.brand}</Card.Title>
+                    <Card.Text className ="card-name">{products.name}</Card.Text> 
+                    <Card.Text className ="card-dollarsign">{products.price_sign}{products.price}  {products.currency}</Card.Text>
+                    <Button variant ="dark"> Buy now </Button>        
+                  </Card.Body>
+              </Card>
+              </div>
+            );
+          })}
     </div>
-  );
+  )
 }
 
+//This exports the 'Allproducts' component as the default export of the module allowing it to be imported and used in other parts of the codebase
 export default Allproducts;
